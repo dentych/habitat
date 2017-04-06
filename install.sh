30da8cb6c5cb9b7e4179be724ced749bfc70476f
dentych <dennis@tychsen.me>
2017-04-06 19:33:14 +0200

    Make sure git-prompt is loaded from home directory

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ git config --global alias.b "branch"
 if [[ $SETUPECHO = true ]]; then
     echo "Setting up PS1..."
 fi
-source git-prompt.sh
+source ~/git-prompt.sh
 PS1='\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[92m\]\$(__git_ps1 " (%s)")\[\e[00m\] $ '
 if [[ $SETUPECHO = true ]]; then
     echo "Bash setup complete!"
