62ef65f3e03959978a4fdb7a12b0c494415c976b
dentych <dennis@tychsen.me>
2021-07-29 18:27:23 +0200

    Add scrape and initial keybindings.json and settings.json for vscode

--- vscode/vscode.go
+++ vscode/vscode.go
@@ -3,16 +3,30 @@ package vscode
 import (
 	"fmt"
 	"log"
+	"os"
 	"runtime"
 )
 
 func ScrapeVSCodeConfig(homeDir string) {
+	fmt.Println("Scraping VS Code configuration files")
 	keybindingPath := getKeybindingPath(homeDir)
 	settingsPath := getSettingsPath(homeDir)
 
 	if keybindingPath == "" || settingsPath == "" {
 		log.Fatalf("Unsupported platform detected: %s\n", runtime.GOOS)
 	}
+
+	keybindingJson, err := os.ReadFile(keybindingPath)
+	if err != nil {
+		log.Fatal("Failed to read keybindings:", err)
+	}
+	os.WriteFile("keybindings.json", keybindingJson, 0644)
+
+	settingsJson, err := os.ReadFile(settingsPath)
+	if err != nil {
+		log.Fatal("Failed to read settings:", err)
+	}
+	os.WriteFile("settings.json", settingsJson, 0644)
 }
 
 func getKeybindingPath(homeDir string) string {
