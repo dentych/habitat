f0e8c2418ca4e8239aadaf719dd8d88233c2bce5
dentych <dennis@tychsen.me>
2021-07-30 13:13:36 +0200

    Finish installation script

--- vscode/vscode.go
+++ vscode/vscode.go
@@ -6,14 +6,49 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
+
+	_ "embed"
+
+	"gitlab.com/dentych/habitat/terminal"
 )
 
+//go:embed extensions.txt
+var extensions string
+
+//go:embed keybindings.json
+var keybindings []byte
+
+//go:embed settings.json
+var settings []byte
+
 func InstallVSCode(homeDir string) {
-	fmt.Println("Creating VSCode settings directory if it doesn't exist: ", getVsCodeSettingsDirectory(homeDir))
-	err := os.MkdirAll(getVsCodeSettingsDirectory(homeDir), 0755)
+	settingsDir := getVsCodeSettingsDirectory(homeDir)
+	fmt.Println("Creating VSCode settings directory if it doesn't exist: ", settingsDir)
+	err := os.MkdirAll(settingsDir, 0755)
 	if err != nil {
 		log.Fatalln("There was an error making the VsCode settings directory: ", err)
 	}
+
+	keyBindingPath := getKeybindingPath(settingsDir)
+	settingsPath := getSettingsPath(settingsDir)
+
+	fmt.Println("Writing keybindings.json...")
+	err = os.WriteFile(keyBindingPath, keybindings, 0644)
+	if err != nil {
+		log.Fatalln("Failed to write keybindings.json file: ", err)
+	}
+	fmt.Println("keybindings.json successfully written")
+
+	fmt.Println("Writing settings.json...")
+	err = os.WriteFile(settingsPath, settings, 0644)
+	if err != nil {
+		log.Fatalln("Failed to write settings.json file: ", err)
+	}
+	fmt.Println("settings.json successfully written")
+
+	fmt.Println("Installing extensions...")
+	installExtensions()
 }
 
 func ScrapeVSCode(homeDir string) {
@@ -67,4 +102,37 @@ func getKeybindingPath(settingsDir string) string {
 
 func getSettingsPath(settingsDir string) string {
 	return fmt.Sprintf("%s/settings.json", settingsDir)
+}
+
+func installExtensions() {
+	fmt.Println("Ensuring code command exists")
+	cmd := exec.Command("code", "--version")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalln("Code command not found: ", err)
+	}
+
+	extensionList := strings.Split(strings.TrimSpace(extensions), "\n")
+	fmt.Println("Installing the following extensions:")
+	for _, ext := range extensionList {
+		if len(ext) > 0 {
+			fmt.Println("- " + ext)
+		}
+	}
+
+	for _, ext := range extensionList {
+		if len(strings.TrimSpace(ext)) == 0 {
+			continue
+		}
+
+		terminal.PrintHeading(fmt.Sprintf("Installing extension: %s", ext))
+		cmd := exec.Command("code", "--install-extension", ext)
+		cmd.Stdout = os.Stdout
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalln("Failed to install extensions: ", err)
+		}
+		fmt.Println()
+		fmt.Println()
+	}
 }
